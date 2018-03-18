9a736146894d69edef1555be64a26c5155b5735d
LucasArusiewicz <lucasarusiewicz@gmail.com>
2018-03-18 15:33:03 -0300

    Barra de Dano

---
+++ Projeto Unity/Assets/Modelos/UIBarraDano.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBarraDano : MonoBehaviour {
+
+    public Image barraVida;
+    public float vel = 0.4f;
+    private Image barraDano;
+
+    void Start()
+    {
+        barraDano = GetComponent<Image>();
+    }
+
+    void LateUpdate()
+    {
+
+        if (barraVida.fillAmount < barraDano.fillAmount)
+        {
+            barraDano.fillAmount -= Time.deltaTime;
+        }
+        else
+        {
+            barraDano.fillAmount = barraVida.fillAmount;
+        }
+
+    }
+}
