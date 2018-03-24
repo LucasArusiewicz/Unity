2f686123071fc62ff832c966e30e0b4f04d73249
HuFuzzy <rodrigo.rosa.005@acad.pucrs.br>
2018-03-24 19:50:58 -0300

    Criado evento em cada carta, ao clicar uma UI da respectiva tropa é gerada

--- RTS GIT/Assets/Interface/UIPlayerDeck.cs
+++ RTS GIT/Assets/Interface/UIPlayerDeck.cs
@@ -9,41 +9,50 @@ using UnityEngine.UI;
 public class UIPlayerDeck : MonoBehaviour {
 
 	public MComandante comandante ;
-	 
-	public float TamanhoPTelaH;
-	public float TamanhoPTelaV;
-	public float DeltaV;
-	public float DeltaH;
-	public GameObject canvas;
+	public GameObject panelCartas;
 	public GameObject cartaPrefab;
+    public GameObject PanelTropaSpawned;
+	public GameObject UITropa;
 
+    private ArrayList cartasEventos = new ArrayList();
 
+    
 
-	void Start () {
-
-       
-
-
+    void Start () {
 
         foreach (MCarta comandanteCarta in comandante.getDeck()){
 
 			GameObject newCarta = Instantiate(cartaPrefab) as GameObject;
-            newCarta.transform.SetParent(canvas.transform, false);
-        
+            newCarta.transform.SetParent(panelCartas.transform, false);
             newCarta.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = comandanteCarta.getImage();
+
+
+
+            Button buttonCtrl = newCarta.transform.GetChild(1).gameObject.GetComponent<Button>();
+            buttonCtrl.onClick.AddListener(() => criaUI_Tropa(comandanteCarta.getImage()));
+
+            cartasEventos.Add(buttonCtrl);
+
+
+          
 
 
         }
 		
 	}
-	
-	
+
+    public void criaUI_Tropa(Sprite a )
+    {
+        GameObject Tropa = Instantiate(UITropa) as GameObject;
+        Tropa.transform.SetParent(PanelTropaSpawned.transform, false);
+        Tropa.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = a;
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
 
 
 
-
-	void OnGUI() {
-			 
-			
-	}
 }
