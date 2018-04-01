323e9a1815f6c2a3e9e3ae484cc554730fee4408
HuFuzzy <rodrigo.rosa.005@acad.pucrs.br>
2018-03-31 22:24:16 -0300

    criado exemplos de seleção de tropas

---
+++ Projeto Unity/Assets/MTropa.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MTropa : MonoBehaviour {
+
+
+    public Projector projetorDeSelecao;
+    private bool selecionada;
+
+
+
+
+    private void Start(){
+        CTropa.adicionaTropa(this);
+    }
+
+
+
+
+
+    public bool getSelecionada(){
+       return selecionada;
+    }
+
+
+    public void setSelecionada(bool select){
+        if (select){
+            projetorDeSelecao.enabled = true;
+        }
+
+        else {
+            projetorDeSelecao.enabled = false;
+        }
+
+        selecionada = select;
+    }
+
+}
