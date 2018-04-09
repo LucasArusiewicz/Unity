19786be1ccdf0ed6e445bc8ee51fa4d333b64e17
Toniolo <cacaujr@live.com>
2018-04-09 20:55:07 -0300

    Menuzin

--- Projeto Unity/Assets/SettingsMenu.cs
+++ Projeto Unity/Assets/SettingsMenu.cs
@@ -2,13 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour {
 
 	public AudioMixer audioMixer;
 
+	public Dropdown resolutionChange;
+
+	Resolution[] resolutions;
+
+	void Start()
+	{
+		resolutions = Screen.resolutions;
+
+		resolutionChange.ClearOptions();
+
+		List<string> options = new List<string>();
+
+		int actualResolutionValue = 0;
+
+		for(int i = 0; i < resolutions.Length; i++)
+		{
+			string option = resolutions[i].width + " x " + resolutions[i].height;
+
+			options.Add(option);
+
+			if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+			{
+				actualResolutionValue = i;
+			}
+		}
+
+		resolutionChange.AddOptions(options);
+
+		resolutionChange.value = actualResolutionValue;
+
+		resolutionChange.RefreshShownValue();
+	}
+
+	public void setResolution(int resolutionValue)
+
+	{
+		Resolution resolution = resolutions[resolutionValue];
+
+		Screen.SetResolution(resolution.width,resolution.height, Screen.fullScreen);
+	}
+
 	public void setVolume(float volume)
 	{
 		audioMixer.SetFloat("Volume",volume);
 	}
+
+	public void setQuality(int qualityValue)
+	{
+		QualitySettings.SetQualityLevel(qualityValue);
+	}
+
+	public void fullscreenMode(bool fullscreen)
+	{
+		Screen.fullScreen = fullscreen;
+	}
+		
 }
